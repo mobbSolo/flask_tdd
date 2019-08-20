d27c3bbca2d37e64d68fa575d81a86454ebb27d8
markie_tee <cassette.head@gmail.com>
2019-08-19 23:16:53 -0600

    initialize flask app

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+                  abort, render_template, flash, jsonify
+
+# Config
+DATABASE = 'flaskr.db'
+DEBUG = True
+SECRET_KEY = 'my_precious'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
 
 app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+app.config.from_object(__name__)
 
 
 if __name__ == '__main__':
