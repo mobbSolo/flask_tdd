0cca4be1b241fee7822619cb1e7ef1070f0d54e3
markie_tee <cassette.head@gmail.com>
2019-08-19 23:47:19 -0600

    Implement functions for working the database
    connect_db, init_db, get_db, close_db

--- app-test.py
+++ app-test.py
@@ -1,6 +1,7 @@
+import unittest
+import os
 from app import app
 
-import unittest
 
 class BasicTestCase(unittest.TestCase):
 
@@ -8,6 +9,10 @@ class BasicTestCase(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 404)
+
+    def test_database(self):
+        tester = os.path.exists('flaskr.db')
+        self.assertTrue(tester)
 
 
 if __name__ == '__main__':
